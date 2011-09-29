af6d7541f4c377ceb70c571aad28586403ebfc6e
Dipil <dipil@dipil-XPS-L501X.(none)>
2011-09-29 23:16:24 +0545

    Initial empty app set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CfLineCreateGui::Application.config.secret_token = 'cb07ba92a614086e32672eeb62cd1b68a5f5844e1eddc19cdf16714fa4e562737eacba08130fe258633f109474b423045fcc4dfd99d2a0977f0597f62b6d69cc'
